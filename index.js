943b467001dfd812de29ebba6d338030960120cf
William A. Ferguson <wilsb8@gmail.com>
2023-04-26 10:56:13 +0000

    server and routes

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http"); // require http
+const app = require("./app/app");
+// require(ment) declaration
+require("dotenv").config(); // read from our .env file
+require('./app/app')
+
+// create listen server
+http.createServer(app).listen(process.env.port, ()=> {
+    console.log(`Server here. I am fully operational and listening on port: ${process.env.port}`)
+});
